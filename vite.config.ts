3add871a26848667c9663a9889dd3ad5277ddec0
skdaniil <161622151+skdaniil@users.noreply.github.com>
2024-03-06 12:57:01 +0300

    Added AppTable

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,9 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()],
+    }),
+    vueJsx({
+      // options are passed on to @vue/babel-plugin-jsx
     }),
   ],
   resolve: {
