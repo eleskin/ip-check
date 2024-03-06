55088f81a3a901eb6f89fa832fdc647f79b436b8
skdaniil <161622151+skdaniil@users.noreply.github.com>
2024-03-06 13:58:51 +0300

    Added searchbox

--- src/components/AppTable.vue
+++ src/components/AppTable.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="AppTable">
     <div class="container AppTable__container">
+        <el-input
+          v-model="searchValue"
+          style="width: 240px"
+          placeholder="Поиск по таблице..."
+          :prefix-icon="Search"
+        />
       <el-auto-resizer>
         <template #default="{ height, width }">
           <el-table-v2
+            v-model:sort-state="sortState"
             :columns="columns"
             :data="data"
             :width="width"
             :height="height"
             fixed
+            @column-sort="onSort"
           />
         </template>
       </el-auto-resizer>
@@ -17,22 +25,15 @@
 </template>
 
 <script lang="tsx" setup>
-import { ref, unref, watch, computed } from 'vue'
-import { ElCheckbox } from 'element-plus'
+import { ref, unref, watch } from 'vue'
+import { TableV2SortOrder, ElCheckbox } from 'element-plus'
 
-import type { FunctionalComponent } from 'vue'
-import type { CheckboxValueType, Column } from 'element-plus'
-
-type SelectionCellProps = {
-  value: boolean
-  intermediate?: boolean
-  onChange: (value: CheckboxValueType) => void
-}
+const searchValue = ref('')
 
 const props = defineProps(['modelValue'])
 const emit = defineEmits(['update:modelValue'])
 
-const SelectionCell: FunctionalComponent<SelectionCellProps> = ({
+const SelectionCell = ({
   value,
   intermediate = false,
   onChange,
@@ -47,7 +48,7 @@ const SelectionCell: FunctionalComponent<SelectionCellProps> = ({
 }
 
 const generateData = (
-  columns: ReturnType<typeof columns>,
+  columns,
   ipList,
 ) =>
   ipList.map((ip, index) => {
@@ -60,23 +61,23 @@ const generateData = (
     )
   })
 
-const columns: Column<any>[] = [
-  {key: `column-0`, dataKey: 'query', title: `IP`, width: 150},
-  {key: `column-1`, dataKey: 'country', title: `Country`, width: 150},
-  {key: `column-2`, dataKey: 'city', title: `City/Town`, width: 150},
-  {key: `column-3`, dataKey: 'status', title: ``, width: 150},
+const columns = [
+  {key: 'column-0', dataKey: 'query', title: 'IP', width: 150},
+  {key: 'column-1', dataKey: 'country', title: 'Country', width: 150},
+  {key: 'column-2', dataKey: 'city', title: 'City/Town', width: 150},
+  {key: 'column-3', dataKey: 'status', title: 'Status', width: 150},
 ]
 columns.unshift({
   key: 'selection',
   width: 50,
   cellRenderer: ({ rowData }) => {
-    const onChange = (value: CheckboxValueType) => (rowData.checked = value)
+    const onChange = (value) => (rowData.checked = value)
     return <SelectionCell value={rowData.checked} onChange={onChange} />
   },
 
   headerCellRenderer: () => {
     const _data = unref(data)
-    const onChange = (value: CheckboxValueType) =>
+    const onChange = (value) =>
       (data.value = _data.map((row) => {
         row.checked = value
         return row
@@ -95,6 +96,24 @@ columns.unshift({
 })
 
 const data = ref(generateData(columns, props.modelValue))
+
+columns[0].sortable = true;
+columns[1].sortable = true;
+columns[2].sortable = true;
+columns[3].sortable = true;
+columns[4].sortable = true;
+
+const sortState = ref<SortState>({
+  'column-0': TableV2SortOrder.ASC,
+  'column-1': TableV2SortOrder.ASC,
+  'column-2': TableV2SortOrder.ASC,
+  'column-3': TableV2SortOrder.ASC,
+})
+
+const onSort = ({ key, order }: SortBy) => {
+  sortState.value[key] = order
+  data.value = data.value.reverse()
+}
 
 watch(() => props.modelValue, (value) => {
   data.value = generateData(columns, value)
